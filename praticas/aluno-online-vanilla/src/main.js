70ea7fb775aeb1990452e9b3dc04d120c8a87664
Gabriell <gabriell.pereira2101@gmail.com>
2026-03-24 09:28:56 -0300

    feat: conclui pratica03. Fecha #5

--- praticas/aluno-online-vanilla/src/main.js
+++ praticas/aluno-online-vanilla/src/main.js
@@ -1,1 +1,29 @@
-import './style.css'
+const form = document.getElementById("loginForm");
+const email = document.getElementById("email");
+const senha = document.getElementById("senha");
+
+const emailError = document.getElementById("emailError");
+const senhaError = document.getElementById("senhaError");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  emailError.textContent = "";
+  senhaError.textContent = "";
+
+  let valido = true;
+
+  if (email.value.trim() === "") {
+    emailError.textContent = "O campo de email é obrigatório.";
+    valido = false;
+  }
+
+  if (senha.value.trim() === "") {
+    senhaError.textContent = "O campo de senha é obrigatório.";
+    valido = false;
+  }
+
+ if (valido) {
+  window.location.href = "index.html";
+}
+});
